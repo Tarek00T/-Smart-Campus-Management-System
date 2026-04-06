619ffdbd2616d06022488235c673cce390b3399c
Md Tarek Miah <tarekmucse2@gmail.com>
2026-04-06 12:38:12 +0600

    updated

---
+++ authentication.js
@@ -0,0 +1,31 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+exports.register = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashed,
+    role
+  });
+
+  res.json(user);
+};
+
+exports.login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.send("User not found");
+
+  const isMatch = await bcrypt.compare(req.body.password, user.password);
+  if (!isMatch) return res.send("Wrong password");
+
+  const token = jwt.sign({ id: user._id, role: user.role }, "secret");
+
+  res.json({ token, role: user.role });
+};
